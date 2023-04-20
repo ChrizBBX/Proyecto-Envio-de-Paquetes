1b6d14477217e243974f39a4ec524036aa3f47a5
Francisco <125476125+ThaPanxho@users.noreply.github.com>
2023-04-20 05:26:43 -0600

    ksshsyta

---
+++ Flutter_Application/flutter_application_1/lib/screens/La_ultima_supongo.dart
@@ -0,0 +1,87 @@
+class MiDato {
+  final String id;
+  final String nombre;
+  final String descripcion;
+
+  MiDato({required this.id, required this.nombre, required this.descripcion});
+}
+
+final misDatos = [
+  MiDato(
+    id: '1',
+    nombre: 'Dato 1',
+    descripcion: 'Descripción del dato 1',
+  ),
+  MiDato(
+    id: '2',
+    nombre: 'Dato 2',
+    descripcion: 'Descripción del dato 2',
+  ),
+  MiDato(
+    id: '3',
+    nombre: 'Dato 3',
+    descripcion: 'Descripción del dato 3',
+  ),
+];
+
+ListView.builder(
+  itemCount: misDatos.length,
+  itemBuilder: (BuildContext context, int index) {
+    return Card(
+      child: InkWell(
+        onTap: () {
+          // Navegar a la pantalla de detalles cuando se haga clic en la tarjeta
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => DetallesScreen(dato: misDatos[index]),
+            ),
+          );
+        },
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                misDatos[index].nombre,
+                style: TextStyle(fontSize: 18.0),
+              ),
+              SizedBox(height: 8.0),
+              Text(misDatos[index].descripcion),
+            ],
+          ),
+        ),
+      ),
+    );
+  },
+);
+
+class DetallesScreen extends StatelessWidget {
+  final MiDato dato;
+
+  DetallesScreen({required this.dato});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(dato.nombre)),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              dato.nombre,
+              style: TextStyle(fontSize: 18.0),
+            ),
+            SizedBox(height: 8.0),
+            Text(dato.descripcion),
+            SizedBox(height: 16.0),
+            Text('ID: ${dato.id}'),
+          ],
+        ),
+      ),
+    );
+  }
+}
